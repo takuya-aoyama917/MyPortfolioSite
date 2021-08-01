e1b419f8d00946177f0f354a8570359b8f7c7172
青山　拓也 <70190159+takuya-aoyama917@users.noreply.github.com>
2021-08-01 19:14:53 +0900

    initial commit

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Profile, Work, Experience, Education, Software, Technical
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Work)
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Software)
+admin.site.register(Technical)
+
